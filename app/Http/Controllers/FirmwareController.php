de49827df034b5f1d8135303ae4702f75905a36e
flumeded <nikita@dymko.com>
2019-10-11 06:52:32 +0300

    Add upload validation

--- app/Http/Controllers/FirmwareController.php
+++ app/Http/Controllers/FirmwareController.php
@@ -4,39 +4,47 @@ namespace App\Http\Controllers;
 
 use http\Env\Response;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
-use Symfony\Component\Process\Process;
 use Webpatser\Uuid\Uuid;
-use Illuminate\Support\Facades\Storage;
+
+
+
 
 
 
 class FirmwareController extends Controller
 {
 
-	public function upload(Request $Request)
-	{
+	public function upload(Request $request)
+    {
+        $Session = $request->session();
 
-		$Session = $Request->session();
+        $key = $Session->get("uniqueId");
 
-		$key = $Session->get("uniqueId");
+        if (empty($key)) {
+            $key = Uuid::generate(4);
+            $Session->put("uniqueId", $key);
+        }
 
-		if (empty($key)) {
-			$key = Uuid::generate(4);
-			$Session->put("uniqueId", $key);
-		}
+        $filename = $key;
+        $downloadDir = 'firmwares/%s';
+        $userStorageDir = sprintf($downloadDir, $filename);
+        $requestKey = 'firmware';
+        if ($request->hasFile($requestKey)) {
 
-		$filename = $key;
-		$downloadDir = 'firmwares/%s';
-		$userStorageDir = sprintf($downloadDir, $filename);
-		$requestKey = 'firmware';
-		$fullFilePath = "../storage/app/" . $Request->file($requestKey)->storeAs($userStorageDir, $filename);
-		//Assembling full execution command string with relative path
-		$cmdTpl = '/usr/bin/avr-objcopy -I ihex %s -O binary %s.bin';
-		$cmd = sprintf($cmdTpl, escapeshellarg($fullFilePath), escapeshellarg($fullFilePath));
-		exec($cmd, $output, $exitCode);
-        return response()->download ($fullFilePath . '.bin', 'FLASH.bin');
+            $fullFilePath = "../storage/app/" . $request->file($requestKey)->storeAs($userStorageDir, $filename);
+            //Assembling full execution command string with relative path
+            $cmdTpl = '/usr/bin/avr-objcopy -I ihex %s -O binary %s.bin';
+            $cmd = sprintf($cmdTpl, escapeshellarg($fullFilePath), escapeshellarg($fullFilePath));
+            exec($cmd, $output, $exitCode);
+            return response()->download($fullFilePath . '.bin', 'FLASH.bin');
+        }
+        else {
 
-	}
+            notify()->error('Oopsie', 'Please select file to upload');
+            return view('home');
+
+        }
+
+    }
 
 }
